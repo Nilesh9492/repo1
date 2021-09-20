1626f5e0b2d9cc7a7a64959b7b312ea718d4e37d
Nilesh9492 <nilesh94041@gmail.com>
2021-09-20 16:52:07 +0530

    array

--- sortingarray.sh
+++ sortingarray.sh
@@ -1,8 +1,17 @@
+declare -A math;
 read -p "Enter value a:" a;
 read -p "Enter value b:" b;
 read -p "Enter value c:" c;
-comp1=$((a+b*c));
-comp2=$((a*b+c));
-comp3=$((a*c+b));
+
+math[comp1]=$((a+b*c));
+math[comp2]=$((a*b+c));
+math[comp3]=$((a*c+b));
+
+
+for ((cnt=0;cnt<${#math[@]};cnt++))
+do
+	varArray[cnt]=${math[comp"$(( cnt+1 ))"]};
+done
 echo $a $b $c;
-echo $comp1 $comp2 $comp3
+echo ${math[@]};
+echo ${varArray[@]};
